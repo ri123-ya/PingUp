90d5d54ffbc7dda7d2083413f5c6aa08f70a84c0
Riya Rastogi <riya02rastogi@gmail.com>
2025-04-07 18:43:10 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,12 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate  } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 
 const App = () => {
@@ -42,6 +43,8 @@ const App = () => {
          <Route path="/settings" element={<SettingsPage />} />
          <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
+      <Toaster />
     </div>
   )
 }
