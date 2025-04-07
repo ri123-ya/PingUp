87e262cd01878d3872483604d8d142e3cc5bbd45
Riya Rastogi <riya02rastogi@gmail.com>
2025-04-07 18:44:42 +0530

    state management  for login and logout

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,4 +1,7 @@
 import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios.js";
+
 
 export const useAuthStore = create((set) => ({
     authUser: null,
@@ -21,4 +24,44 @@ export const useAuthStore = create((set) => ({
         }
         
     },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try{
+            const res = await axiosInstance.post("/auth/signup", data);
+            toast.success("Account created successfully");
+            set({ authUser: res.data });
+
+        } catch (error){
+            toast.error("error.respinse.data.message");
+        } finally {
+            set({ isSigningUp: false});
+        }
+
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+          const res = await axiosInstance.post("/auth/login", data);
+          set({ authUser: res.data });
+          toast.success("Logged in successfully");
+    
+          get().connectSocket();
+        } catch (error) {
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isLoggingIn: false });
+        }
+    },
+
+    logout: async (data)=> {
+        try {
+            const res = await axiosInstance.post("/auth/logout");
+            set({ authUser: null});
+            toast.success("Logged put successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    }
 }))
