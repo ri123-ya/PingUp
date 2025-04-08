f477ed4a8368c5de550f47948b3ebe4a1766fad7
Riya Rastogi <riya02rastogi@gmail.com>
2025-04-08 16:37:40 +0530

    initial commit

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -4,10 +4,17 @@ import { config } from "dotenv";
 
 config();
 
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
+// console.log("🔍 ENV CHECK:");
+// console.log("CLOUDINARY_CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+// console.log("CLOUDINARY_API_KEY:", process.env.CLOUDINARY_API_KEY);
+// console.log("CLOUDINARY_API_SECRET:", process.env.CLOUDINARY_API_SECRET);
 
 export default cloudinary;
