527efa62de01f703509823f2a45894c1954257d5
Riya Rastogi <riya02rastogi@gmail.com>
2025-04-08 16:38:40 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from "./pages/ProfilePage";
 
 import { Routes, Route, Navigate  } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
@@ -15,8 +16,12 @@ import { Toaster } from "react-hot-toast";
 
 
 const App = () => {
-  const {authUser , checkAuth, isCheckingAuth} =useAuthStore()
+  const {authUser , checkAuth, isCheckingAuth, onlineUsers} =useAuthStore()
+  
+  const {theme} = useThemeStore()
 
+  console.log({ onlineUsers });
+  
   useEffect(() => {
     checkAuth()
   },[checkAuth]);
@@ -33,7 +38,7 @@ const App = () => {
   
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar/>
 
       <Routes>
